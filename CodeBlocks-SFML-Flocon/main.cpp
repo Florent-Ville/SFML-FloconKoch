f3bd9fedf2d7ba9ef425addd37b9530162016a9a
Florent <florentvilleneuve1@gmail.com>
2021-11-24 22:06:58 +0100

    Fonctionnel

--- CodeBlocks-SFML-Flocon/main.cpp
+++ CodeBlocks-SFML-Flocon/main.cpp
@@ -10,7 +10,6 @@
 
 void generationRecursive(Point A, Point B, int niveau, std::stack<sf::VertexArray>& pileListe)
 {
-
     if(niveau == 0)
     {
         sf::VertexArray lignes(sf::Lines, 2);
@@ -19,44 +18,35 @@ void generationRecursive(Point A, Point B, int niveau, std::stack<sf::VertexArra
         pileListe.push(lignes);
 
     }else{
-
     Ligne ligne(A, B);
     Point C, D, E;
     ligne.calculerPoints();
     C = ligne.getPointC();
 
-
-    std::cout << "C.x = " << C.getX();
-    std::cout << "C.y = " << C.getY();
+    //C.setY(-C.getY());
 
 
     D = ligne.getPointD();
+    //D.setY(-D.getY());
 
-    std::cout << "D.x = " << D.getX();
-    std::cout << "D.y = " << D.getY();
 
     E = ligne.getPointE();
 
-    std::cout << "E.x = " << E.getX();
-    std::cout << "E.y = " << E.getY() << std::endl;
+    //std::cout << "E.x = " << E.getX();
+   // std::cout << "E.y = " << E.getY() << std::endl;
 
   /*  generationRecursive(A, C, niveau - 1, pileListe);
     generationRecursive(C, E, niveau - 1, pileListe);
     generationRecursive(E, D, niveau - 1, pileListe);
-    generationRecursive(D, B, niveau - 1, pileListe); */
+    generationRecursive(D, B, niveau - 1, pileListe);*/
 
-    generationRecursive(C, A, niveau - 1, pileListe);
-    generationRecursive(E, C, niveau - 1, pileListe);
-    generationRecursive(D, E, niveau - 1, pileListe);
-    generationRecursive(B, D, niveau - 1, pileListe);
+    generationRecursive(D, B, niveau - 1, pileListe);
+    generationRecursive(E, D, niveau - 1, pileListe);
+    generationRecursive(C, E, niveau - 1, pileListe);
+    generationRecursive(A, C, niveau - 1, pileListe);
 
     }
-
 }
-
-
-
-
 
 int main()
 {
@@ -74,8 +64,12 @@ int main()
 
 
     //Initialisation
-    Point A(100.f,100.f);
-    Point B(500.f,100.f);
+    Point A(100.f,300.f);
+    Point B(600.f,300.f);
+
+    //Point C(50.f,70);
+
+
 
 
     // Start the game loop
@@ -93,78 +87,17 @@ int main()
         window.clear();
 
 
-        //Ligne de Test
-        sf::Vertex line[] =
-            {
-            sf::Vertex(sf::Vector2f(10.f, 10.f)),
-            sf::Vertex(sf::Vector2f(150.f, 150.f))
-            };
-
-        //window.draw(line, 2, sf::Lines);
-
-
-        //Graphe Manuelle
-
-    /*    Ligne L(A, B);
-
-        Point Aprime = L.getPointA();
-        Point Bprime = L.getPointB();
-
-        float truc = Aprime.getX();
-        float truc2 = Aprime.getY();
-        float truc3 = Bprime.getX();
-        float truc4 = Bprime.getY();
-
-        sf::Vertex line2[] =
-            {
-            sf::Vertex(sf::Vector2f(truc, truc2)),
-            sf::Vertex(sf::Vector2f(truc3, truc4))
-            };
-
-        window.draw(line2, 2, sf::Lines);*/
-
-        //std::list<sf::VertexArray> lignesliste;
-        //std::list<sf::VertexArray>::iterator iterateur;
-
         std::stack<sf::VertexArray> lignesliste;
 
 
-        sf::VertexArray lignes(sf::Lines, 2);
-        lignes[0].position = sf::Vector2f(A.getX(), A.getY());
-        lignes[1].position = sf::Vector2f(B.getX(), B.getY());
+        int niveau = 3;
 
-        sf::VertexArray lignes2(sf::Lines, 2);
-        lignes2[0].position = sf::Vector2f(200.f, 88.f);
-        lignes2[1].position = sf::Vector2f(678.f , 189.f);
-
-        //window.draw(lignes);
-       // window.draw(lignes2);
-
-  /*      lignesliste.push(lignes);
-        lignesliste.push(lignes2);
-
+        generationRecursive(A, B, niveau, lignesliste);
+       // generationRecursive(B, C, niveau, lignesliste);
+       // generationRecursive(C, A, niveau, lignesliste);
 
         while (!lignesliste.empty())
         {
-
-            window.draw(lignesliste.top());
-            lignesliste.pop();
-        } */
-
-
-         //window.draw(lignes);
-
-
-
-
-
-
-
-        generationRecursive(A, B, 1, lignesliste);
-
-        while (!lignesliste.empty())
-        {
-
             window.draw(lignesliste.top());
             lignesliste.pop();
         }
--- CodeBlocks-SFML-Flocon/src/Ligne.cpp
+++ CodeBlocks-SFML-Flocon/src/Ligne.cpp
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 
-#define SOIXANTE_DEGRES 1.04719755
+#define SOIXANTE_DEGRES 1.0471975512
 
 Ligne::Ligne(Point A, Point B)
 {
@@ -65,53 +65,45 @@ void Ligne::calculerPoints()
     float distance = distancePoints(_A, _B);
     float angle = anglePoints(_A, _B);
 /*
-     Point U(_B.getX() - _A.getX() , _B.getY() - _A.getY()); //Vecteur https://stackoverflow.com/questions/15367165/finding-coordinates-of-koch-curve
-     Point V(_A.getY() - _B.getY() , _B.getX() - _A.getX()); //Vecteur
 
-    _C.setX(_A.getX() + ((1/3)* U.getX()));
-    _C.setY(_A.getY() + ((1/3)* U.getY()));
+    if(_A.getY() == _B.getY()){
+        _C.setX(_A.getX() + distance/3*cos(angle));
+        _C.setY(_A.getY() + distance/3*sin(angle));
 
-    _D.setX(_A.getX() + ((2/3)* U.getX()));
-    _D.setY(_A.getY() + ((2/3)* U.getY()));
+        _D.setX(_A.getX() + 2*distance/3*cos(angle));
+        _D.setY(_A.getY() + 2*distance/3*sin(angle));
 
-    _E.setX(_A.getX() + ((1/2)* U.getX()) + ((sqrt(3)/6)*V.getX()));
-    _E.setY(_A.getY() + ((1/2)* U.getY()) + ((sqrt(3)/6)*V.getY()));*/
+        _E.setX(_C.getX() + distance/3*cos(angle + SOIXANTE_DEGRES));
+        _E.setY(_C.getY() + distance/3*sin(angle + SOIXANTE_DEGRES));
 
+     }else if(_A.getX() < _B.getX()){
+        _C.setX(_A.getX() + (_A.getX() + distance)/3);
+        _C.setY(_A.getY() + (_A.getY() + distance)/3);
 
-    _C.setX(_A.getX() + distance/3*cos(angle));
-    _C.setY(_A.getY() + distance/3*sin(angle));
+        _D.setX(_A.getX() + 2*(_A.getX() + distance)/3);
+        _D.setY(_A.getY() + 2*(_A.getY() + distance)/3);
 
-    _D.setX(_A.getX() + 2*distance/3*cos(angle));
-    _D.setY(_A.getY() + 2*distance/3*sin(angle));
-
-    _E.setX(_C.getX() + distance/3*cos(angle + SOIXANTE_DEGRES));
-    _E.setY(_C.getY() + distance/3*sin(angle + SOIXANTE_DEGRES));
-
-
-
-    /*/Ca où j'ai une ligne parfaitement horizontale/*/
- /*   if(_A.getY() == _B.getY()){
-
-    _C.setX(_A.getX() + distance/3*cos(angle));
-    _C.setY(_A.getX() + distance/3*sin(angle));
-
-    _D.setX(_A.getX() + 2*distance/3*cos(angle));
-    _D.setY(_A.getX() + 2*distance/3*sin(angle));
-
-    _E.setX(_C.getX() + distance/3*cos(angle + SOIXANTE_DEGRES));
-    _E.setY(_C.getX() + distance/3*sin(angle + SOIXANTE_DEGRES));
-
-
-    }else if(_A.getX() == _B.getX()){
-
-
-
-    }else{
-
-
-
+        _E.setX(_C.getX() + distance/3*cos(angle + SOIXANTE_DEGRES));
+        _E.setY(_C.getY() + distance/3*sin(angle + SOIXANTE_DEGRES));
     }
 */
+
+        _C.setX(_A.getX() + (_B.getX() - _A.getX())/3);
+        _C.setY(_A.getY() + (_B.getY() - _A.getY())/3);
+
+        _D.setX(_A.getX() + 2*(_B.getX() - _A.getX())/3);
+        _D.setY(_A.getY() + 2*(_B.getY() - _A.getY())/3);
+
+
+        float dX = _D.getX() - _C.getX();
+        float dY = _D.getY() - _C.getY();
+
+        _E.setX((cos(SOIXANTE_DEGRES) * dX + sin(SOIXANTE_DEGRES) * dY) + _C.getX());
+        _E.setY((-sin(SOIXANTE_DEGRES) * dX + cos(SOIXANTE_DEGRES) * dY) + _C.getY());
+
+        //_E.setX(_C.getX() + distance/3*cos(angle + SOIXANTE_DEGRES));
+        //_E.setY(_C.getY() + distance/3*sin(angle + SOIXANTE_DEGRES));
+
 
 
 }
